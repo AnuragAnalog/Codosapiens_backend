c6bce8c1b31e7a980fa5e2566b6150367df711a4
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2019-12-17 13:21:19 +0530

    Update urls.py

--- Codosapiens_backend/urls.py
+++ Codosapiens_backend/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aboutus/',include('aboutus.urls')),
+    path('home/',include('home.urls')),
+    
 ]
